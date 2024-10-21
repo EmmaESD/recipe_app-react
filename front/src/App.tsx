81af26ed3fb058f6dc864b176f8b6b1bef45c787
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-21 10:34:25 +0200

    create function to fetch data into json file

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { travelType } from "./types/travel.type";
+
 function App() {
+  const [travelList, setTravelList] = useState<travelType[]>([]);
+
+  useEffect(() => {
+    fetchTravels();
+  }, []);
+
+  const fetchTravels = async () => {
+    const response = await fetch("http://localhost:5173/travels.json");
+    const data = await response.json();
+    setTravelList(data);
+  };
+
   return (
     <>
       <h1 className="text-violet-500">Hello world</h1>
+
+      {/* List of travel */}
     </>
   );
 }
---
+++ front/src/types/travel.type.ts
@@ -0,0 +1,8 @@
+export type travelType = {
+  id: Number;
+  name: String;
+  city: String;
+  country: String;
+  image: String;
+  description: String;
+};
