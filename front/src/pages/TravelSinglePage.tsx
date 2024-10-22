230c52185bddb050115a229e9dfc2103d2419c45
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-22 11:59:45 +0200

    add fucntion delete, post ... into front

--- front/src/pages/TravelSinglePage.tsx
+++ front/src/pages/TravelSinglePage.tsx
@@ -11,15 +11,29 @@ const TravelSinglePage = () => {
   }, []);
 
   const fetchTravels = async () => {
-    const response = await fetch("http://localhost:5173/travels.json");
+    const response = await fetch(`http://localhost:8000/travels/${id}`);
     const data = await response.json();
-    const travelFilter = data.filter((t: TravelType) => t.id === Number(id));
-    setTravel(travelFilter[0]);
+    setTravel(data);
   };
+
+  const deleteTravel = async () => {
+    const response = await fetch(`http://localhost:8000/travels/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setTravel(data);
+      window.location.reload(); // Rafraîchit la page après la suppression
+    } else {
+      console.error("Erreur lors de la suppression");
+    }
+  };
+
   return (
     <div>
       <img src={travel?.image} alt="" />
       <h1>{travel?.name}</h1>
+      <button onClick={deleteTravel}>Delete</button>
     </div>
   );
 };
